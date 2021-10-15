0d8b169cee72eea24ce8141849ecb6d00547b5e3
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2021-10-15 16:44:38 +0100

    touches

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+// function getinfo(){
+//     var fname = window.localStorage.getItem('fname');
+//     var infoarea = document.getElementById('fname');
+//     infoarea.innerHTML= fname.toString();
+//     var lname = document.getElementById('lname');
+//     var infoarea = document.getElementById('lname');
+//     infoarea.innerHTML = lname.toString();
+//     var subjectarea = document.getElementById('subject');
+//     var subject = JSON.parse(localStorage.getItem('subject')|| []);
+//     subject.map(subject);
+//     var newsubject = document.createElement('p');
+//     newsubject.innerHTML = subject;
+//     subjectarea.appendChild(newsubject);
+// };
+// function createInfo(){
+//     const value = document.getElementById('subject').value;
+//     var infoarea = document.getElementById('subject');
+//     const newsubject = document.createElement('p');
+//     newsubject.innerHTML = value;
+//     infoarea.appendChild('newsubject');
+//     var subject = JSON.parse(
+//         window.localStorage.getItem('subject') || []);
+//         subject.push(value);
+//         window.localStorage.setItem('subject',JSON.stringify(subject))
+    
+// }
+
+
+
 function showMessage() {
     var fname = $("#fname").val()
     alert("Thanks" + " "+ fname + " " +  "For Your Time We Will Reach To You As Soon As Possible.")
